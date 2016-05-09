7505a726df6ae66360d58da77d73dae8684158ec
dbcdeathdreamer <forgotten@mail.bg>
2016-05-09 22:08:31 +0300

    template

---
+++ admin/common/sidebar.php
@@ -0,0 +1,12 @@
+<!-- start: Main Menu -->
+<div id="sidebar-left" class="span2">
+    <div class="nav-collapse sidebar-nav">
+        <ul class="nav nav-tabs nav-stacked main-menu">
+            <li><a href="index.php"><i class="icon-bar-chart"></i><span class="hidden-tablet"> Dashboard</span></a></li>
+            <li><a href="usersListing.php"><i class="icon-envelope"></i><span class="hidden-tablet"> Users</span></a></li>
+            <li><a href="toursListing.php"><i class="icon-tasks"></i><span class="hidden-tablet"> Tours</span></a></li>
+            <li><a href="blogListing.php"><i class="icon-eye-open"></i><span class="hidden-tablet"> Blog</span></a></li>
+        </ul>
+    </div>
+</div>
+<!-- end: Main Menu -->
---
+++ admin/index.php
@@ -0,0 +1,19 @@
+<?php require_once 'common/header.php'?>
+<?php require_once 'common/sidebar.php'?>
+	<!-- start: Content -->
+	<div id="content" class="span10">
+			<ul class="breadcrumb">
+				<li>
+					<i class="icon-home"></i>
+					<a href="index.html">Home</a> 
+					<i class="icon-angle-right"></i>
+				</li>
+				<li><a href="#">Dashboard</a></li>
+			</ul>
+
+
+
+		
+	</div><!--/.fluid-container-->
+
+<?php require_once 'common/footer.php'?>
